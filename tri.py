da84f96eff5bd9c683eaa9397100874cad0a46e1
toto <narmalone.game@gmail.com>
2020-11-17 10:27:01 +0100

    Update tri.py

--- tri.py
+++ tri.py
@@ -1,1 +1,18 @@
 
+def _init_(hauteur, largeur):
+
+    listOfList=[]
+
+    for i in range(hauteur):
+        listRow = []
+
+        for j in range(largeur):
+            listRow.append("X")
+        listOfList.append(listRow)
+
+    return listOfList   
+
+liste = _init_(5, 5)
+
+for i in liste :
+    print(i)
