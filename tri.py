dcafabe4c3997ba5ebd44c6e4434679487b0a599
toto <narmalone.game@gmail.com>
2020-11-24 12:06:36 +0100

    Update tri.py

--- tri.py
+++ tri.py
@@ -1,9 +1,48 @@
-import random
+#Démineur
+
+#variables
+
+import random 
+
+#Terrain
 
 hauteur = 5
 largeur =5
 
+#Mines
 
+mineMax=5
+
+#Fonction mines
+
+def mines(max):
+    nbMines= random.randint(2, mineMax)
+    return nbMines
+print("il y'a ", mines(mineMax), " mines ")
+
+def _mines_(hauteur, largeur):
+
+    listOfList=[] 
+       
+    for i in range(hauteur):
+        listRow = []
+
+        for j in range(largeur):
+            listRow.append(mines)
+            print(mines)
+        listOfList.append(listRow)
+        
+    
+    return listOfList 
+
+def minesPosition(list):
+    affiche(_init_)
+
+    if modifyTableau==_mines_:
+        print("vous avez perdu")
+
+
+#Fonction terrain
 
 def _init_(hauteur, largeur):
 
@@ -19,14 +58,20 @@ def _init_(hauteur, largeur):
     
     return listOfList 
 
+#postion joueur
+
 def modifyTableau(list):
-    a = int(input("Entrez l'axe des ordonnées"))-1
-    b = int(input("Entre l'axe des abscisses"))-1
+    a = int(input("Choisissez votre ligne : "))-1
+    b = int(input("Choisissez votre colonne : "))-1
     list[a][b]="0"
+
+#Affichage du tableau
 
 def affiche(tableau):
     for i in tableau:
         print(i)
+
+#Si pas perdu continuer
 
 def demineur():
     liste = _init_(hauteur, largeur)
@@ -36,3 +81,4 @@ def demineur():
         affiche(liste)
 
 demineur()
+
